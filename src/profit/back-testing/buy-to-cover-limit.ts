08f47630d3419a09e3e2484527e464b6484fd837
Rogério Albino <roggex@MacBook-Air-de-Rogerio.local>
2023-02-23 13:54:54 -0300

    fix: remove optional from property price

--- src/profit/back-testing/buy-to-cover-limit.ts
+++ src/profit/back-testing/buy-to-cover-limit.ts
@@ -1,21 +1,21 @@
-import { Price, Quantity } from '../types/types';
-import { BaseBackTesting } from './base-back-testing';
+import { Price, Quantity } from "../types/types";
+import { BaseBackTesting } from "./base-back-testing";
 
 /**
  * @class BuyToCoverLimit
  * @summary Envia uma ordem de compra limite para fechar a operação.
  */
 export class BuyToCoverLimit extends BaseBackTesting {
-  private readonly price: Price | undefined;
+  private readonly price: Price;
   private readonly quantity: Quantity | undefined;
 
-  constructor(price?: number, quantity?: number) {
+  constructor(price: number, quantity?: number) {
     super();
     this.price = price;
     this.quantity = quantity;
   }
 
-  public getPrice(): Price | undefined {
+  public getPrice(): Price {
     return this.price;
   }
 
